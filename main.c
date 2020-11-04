8cf57aa6aed94dc6a26d248559b869f1a7736b73
ksuomala <ksuomala@student.hive.fi>
2020-11-05 00:43:21 +0200

    moved functions to main.c file

--- main.c
+++ main.c
@@ -6,11 +6,17 @@
 /*   By: ksuomala <ksuomala@student.hive.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 17:18:41 by ksuomala          #+#    #+#             */
-/*   Updated: 2020/11/05 00:38:36 by ksuomala         ###   ########.fr       */
+/*   Updated: 2020/11/05 00:43:04 by ksuomala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+int		ft_exit(void)
+{
+	exit(0);
+	return (0);
+}
 
 void	ft_errors(int val)
 {
@@ -21,6 +27,23 @@ void	ft_errors(int val)
 	if (val == 3)
 		ft_printf("GNL error");
 	exit(0);
+}
+
+void	ft_init_window(t_map *fdf)
+{
+	fdf->mlx = mlx_init();
+	fdf->win = mlx_new_window(fdf->mlx, WIN_X, WIN_Y, "mlx_window");
+	if (!fdf->img)
+		fdf->img = ft_memalloc(sizeof(t_bitmap));
+	ft_set_scale(fdf, fdf->img);
+	fdf->img->zoom = 1;
+	ft_zero_to_middle(fdf);
+	if (!fdf->img->ptr)
+		fdf->img->ptr = mlx_new_image(fdf->mlx, WIN_X, WIN_Y);
+	if (!fdf->img->b_map)
+		fdf->img->b_map = (int*)mlx_get_data_addr(fdf->img->ptr,\
+		&fdf->img->bpp, &fdf->img->size_l, &fdf->img->endian);
+	fdf->img->size_l /= 4;
 }
 
 int		main(int ac, char **av)
