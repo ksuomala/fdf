238e3390e6b8e38263d8566fd1953b4003c977a9
ksuomala <ksuomala@student.hive.fi>
2020-11-05 00:38:04 +0200

    added color functions

--- colors.c
+++ colors.c
@@ -6,83 +6,89 @@
 /*   By: ksuomala <ksuomala@student.hive.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 22:46:01 by ksuomala          #+#    #+#             */
-/*   Updated: 2020/11/01 18:34:37 by ksuomala         ###   ########.fr       */
+/*   Updated: 2020/11/05 00:37:14 by ksuomala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-//NewColor = sqrt((R1^2+R2^2)/2),sqrt((G1^2+G2^2)/2),sqrt((B1^2+B2^2)/2)
-
-int ft_red(int start, int end)
-{
-	char *asc;
-
-	asc = ft_itoa(start);
-	asc[3] = '\0';
-	start = ft_atoi(asc);
-	free(asc);
-	asc = ft_itoa(end);
-	asc[3] = '\0';
-	end = ft_atoi(asc);
-//	return (sqrt(((start * start) + (end * end)) / 2) * 100000);
-	return (((start + end) / 2) * 1000000);
-}
-
-int ft_green(int start, int end)
-{
-	char *asc;
-
-	asc = ft_itoa(start);
-	asc[6] = '\0';
-	start = ft_atoi(asc + 3);
-	free(asc);
-	asc = ft_itoa(end);
-	asc[6] = '\0';
-	end = ft_atoi(asc + 3);
-//	return (sqrt(((start * start) + (end * end)) / 2) * 1000);
-	return (((start + end) / 2) * 1000);
-}
-
-int ft_blue(int start, int end)
-{
-	char *asc;
-
-	asc = ft_itoa(start);
-	asc[3] = '\0';
-	start = ft_atoi(asc);
-	free(asc);
-	asc = ft_itoa(end);
-	asc[3] = '\0';
-	end = ft_atoi(asc);
-//	return (sqrt(((start * start) + (end * end)) / 2));
-	return (((start + end) / 2));
-}
-int		ft_average_rgb(int start, int end)
-{
-	int red;
-	int green;
-	int blue;
-
-	red = ft_red(start, end);
-	green = ft_green(start, end);
-	blue = ft_blue(start, end);
-//	ft_printf("start|end = |%d|%d|, red = |%d|,green = |%d| blue = |%d| \n", start, end, red, green, blue);
-	//return (red + green + blue);
-	return (0xFF00FF);
-}
-
-int		ft_color(int height)
+int		ft_color_3(int z, int max, int min)
 {
 	int color;
 
-	if (height < 5 && height > -5)
-		color = 0xFFFFFF;
-	else if (height > 4 && height <= 10)
+	if (z > max * 0.7)
 		color = 0xFF0000;
-	else if (height < -5)
-		color = 0x0000FF;
+	else if (z > max / 2)
+		color = 0xFFFF00;
+	else if (z > max / 5)
+		color = 0xFF00;
+	else if (z > max / 10)
+		color = 0x3E418;
+	else if (z < min / 2)
+		color = 0xFF;
+	else if (z < min * 0.7)
+		color = 0xcc;
+	else if (z < 0)
+		color = 0x88;
 	else
-		color = 0x00FF00;
+		color = 0xFFFFFF;
 	return (color);
+}
+
+int		ft_color_simple(int z, int max)
+{
+	int color;
+
+	if (z > max * 0.7)
+		color = 0xFF0000;
+	else
+		color = 0xFF00;
+	if (z < 0)
+		color = 0xFF;
+	if (z >= -5 && z <= 5)
+		color = (0xFFFFFF);
+	return (color);
+}
+
+void	ft_toggle_colors(t_map *fdf)
+{
+	int	y;
+	int x;
+
+	if (fdf->img->c == 4)
+		fdf->img->c = 0;
+	fdf->img->c++;
+	y = 0;
+	x = 0;
+	while (y < fdf->rows)
+	{
+		while (x < fdf->row_len)
+		{
+			if (fdf->img->c == 1)
+				fdf->map[y][x].color = ft_color_simple((int)fdf->map[y][x].z,\
+				fdf->peak_z);
+			else if (fdf->img->c == 2)
+				fdf->map[y][x].color = ft_color_3((int)fdf->map[y][x].z,\
+				fdf->peak_z, fdf->bottom_z);
+			else
+				fdf->map[y][x].color = 0xFF;
+			x++;
+		}
+		y++;
+		x = 0;
+	}
+}
+
+int		ft_average_rgb(int start, int end)
+{
+	unsigned char	r;
+	unsigned char	b;
+	unsigned char	g;
+	int				avg;
+
+	r = ((end + start) / 2 >> 16);
+	b = (unsigned char)((end + start) / 2);
+	g = (unsigned char)((end + start) / 2 >> 8);
+	avg = (b) | (g << 8) | (r << 16);
+	return (avg);
 }
